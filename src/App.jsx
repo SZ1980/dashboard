fdad037e05da53d88f8942a1f36ba62ef1482af9
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-05 22:12:37 +0100

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Topbar from "./components/topbar/Topbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 const App = () => {
   return (
     <div>
       <Topbar />
+
+      <Sidebar />
     </div>
   );
 };
