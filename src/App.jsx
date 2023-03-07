dee07da91758cb177f36bffdbc2fd867f0f0f9b6
Zoran Simonovic <simonovic1980@gmail.com>
2023-03-07 15:00:24 +0100

    create user

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./pages/home/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/home/user/User";
 
 const App = () => {
   return (
@@ -15,8 +16,11 @@ const App = () => {
           <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/user/:userId">
+          <Route path="/users">
             <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
           </Route>
         </Switch>
       </div>
